2e1c0c4f30b883be3f06b1fd8ed8c301638c655d
nanderton <noelanderton@cox.net>
2020-02-21 17:16:24 -0700

    Initial commit

---
+++ src/BlazorRades.State/BlazorRades.State/StateService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRades.State
+{
+    public class StateService
+    {
+        private Dictionary<string, object> state = new Dictionary<string, object>();
+        public void Add<T>(string key, T value)
+        {
+            state.Add(key, value);
+        }
+
+        public T Get<T>(string key)
+        {
+            object value;
+            if(state.TryGetValue(key, out value))
+            {
+                return (T) value;
+            }
+
+            return (T) value;
+        }
+    }
+}
---
+++ src/BlazorRades.State/BlazorRades.StateTests/StateServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlazorRades.State;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorRades.State.Tests
+{
+    [TestClass]
+    public class StateServiceTests
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            var sut = new StateService();
+            var testMessage = new TestMessage();
+            sut.Add<TestMessage>("1", testMessage);
+            var result = sut.Get<TestMessage>("1");
+            Assert.AreEqual(testMessage, result);
+        }
+    }
+
+    internal class TestMessage
+    {
+    }
+}
